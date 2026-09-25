e9cfd8fb400a82a17e1a08bcf31004d759e9ced8
whitek <whitenet@dev.com>
2026-09-25 16:42:39 +0700

    sol2

--- task.sql
+++ task.sql
@@ -1,9 +1,16 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB; 
-CREATE INDEX idx_product_name ON Products(Name);
-CREATE INDEX idx_first_name ON Customers(FirstName);
-CREATE INDEX idx_last_name ON Customers(LastName);
-CREATE INDEX idx_email ON Customers(Email);
-CREATE INDEX idx_orders_customer_id ON Orders(CustomerID);
-CREATE INDEX idx_orderitems_order_id ON OrderItems(OrderID);
-CREATE INDEX idx_orderitems_product_id ON OrderItems(ProductID);
+CREATE INDEX idx_customers_email
+ON Customers(Email);
+
+CREATE INDEX idx_products_name
+ON Products(Name);
+
+CREATE INDEX idx_orders_customer_id
+ON Orders(CustomerID);
+
+CREATE INDEX idx_orderitems_order_id
+ON OrderItems(OrderID);
+
+CREATE INDEX idx_orderitems_product_id
+ON OrderItems(ProductID);
